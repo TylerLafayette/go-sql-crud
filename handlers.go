edba7d938808e9027b112e14bfbbca92d26849d4
Tyler Lafayette <tylerlafayette@gmail.com>
2019-03-21 22:03:29 -0700

    Renamed Init struct, created Options and Field struct. Created GetRow function.

--- handlers.go
+++ handlers.go
@@ -1,8 +1,32 @@
 package sqlcrud
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+)
 
-// Options contains a configuration for the handlers.
-type Options struct {
+// Init contains a configuration for the database connection.
+type Init struct {
 	Database *sql.DB
+}
+
+// Options contains options for a single handler.
+type Options struct {
+	QueryFields []Field
+	Fields      []Field
+}
+
+// Field represents one field to query.
+type Field struct {
+	InputName string
+	Name      string
+	Validator func(string) (string, error)
+	Formatter func(string) (string, error)
+}
+
+// GetRow returns a handler to get one row from the database and return it to the user.
+func (i *Init) GetRow(o Options) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 }
